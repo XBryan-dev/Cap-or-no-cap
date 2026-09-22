fabe736bfaa7ef04c215d1667d2cf9217f6c5ef2
XBryan-dev <ndiwumbryan@gmail.com>
2026-09-22 12:21:20 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Bump this string every time you deploy a change.
 // It's the ONLY thing that needs to change to trigger the
 // "Update available" banner for people who already have the app open.
-const VERSION = 'v1.0.0';
+const VERSION = 'v1.1.0';
 
 const CACHE_NAME = 'cap-or-no-cap-' + VERSION;
 
